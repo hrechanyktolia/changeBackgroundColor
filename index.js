ad67b08e418f80c8601c2fa8ae766a4661551579
Your Name <tolikhrechanyk25@gmail.com>
2022-09-20 16:46:22 +0300

    v1

---
+++ index.js
@@ -0,0 +1,23 @@
+let btn = document.querySelector('.btn')
+let color = document.querySelector('.color')
+
+const colors = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+
+btn.addEventListener('click', () => {
+    let getColor = generateColor()
+    document.body.style.backgroundColor = getColor
+    color.textContent = getColor
+})
+
+const randomNumber = () => {
+    return Math.floor(Math.random() * colors.length)
+}
+
+const generateColor = () => {
+    let getColor = '#'
+    for (let i = 0; i < 6; i++) {
+        getColor += colors[randomNumber()]
+    }
+    return getColor
+}
